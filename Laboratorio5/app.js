29fd9fee3c2647feabf46be54fbb9460305c28c2
root <root@kali>
2020-06-02 01:21:17 -0400

    Comments on index.js

--- Laboratorio5/app.js
+++ Laboratorio5/app.js
@@ -4,23 +4,39 @@ const express = require('express');
 const app = express();
 const st = new Estudiantes();
 
+/**
+ * Brings all students
+ */
 app.get('/estudiantes', (req, resp) => {
     resp.send(JSON.stringify(st.sudents))
 });
 
+/**
+ * Brings specific student
+ */
 
 app.get('/estudiantes/:id', (req, resp) => {
     let ans = st.getSpecificStudent(req.params.id)
     resp.send(JSON.stringify(ans))
 });
 
+/**
+ * Creates a new student
+ */
+
 app.post('/estudiantes', (req, resp) => {
     resp.send('This function is not working yet, sorry')
 });
 
+/**
+ * Edits one student
+ */
 app.put('/estudiantes/:id', (req, resp) => {
     resp.send(`This function is not working yet, sorry: ${req.params.id}`)
 });
+/**
+ * Deletes one student
+ */
 
 app.delete('/estudiantes/:id', (req, resp) => {
     let ans = st.deleteStudent(req.params.id)
